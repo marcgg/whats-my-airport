8b296c9873c6901d6fb6878b9df46e83022efcf3
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-12-27 16:27:43 +0100

    better routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to result_path(id: @user.id)
+      redirect_to result_path(id: @user.id, name: @user.name.parameterize)
     else
-      flash[:error] = "loleno"
-      redirect_to "/"
+      redirect_to root_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AirportCodes::Application.routes.draw do
   root :to => "users#new"
-  match "/r/:id" => "users#show", :as => :result
+  match "/:name/:id" => "users#show", :as => :result
   resources :users, :only => [:create]
 end
