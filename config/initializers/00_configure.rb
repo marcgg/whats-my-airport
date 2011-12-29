4eb32c4065c1b83e67a4ddb793e7725165f7795f
Guillaume Flandre <guiome.flandre@gmail.com>
2011-12-29 22:25:34 +0100

    handles config for Heroku

--- config/initializers/00_configure.rb
+++ config/initializers/00_configure.rb
@@ -1,4 +1,10 @@
-puts "Loading config variables from services.yml"
-config = YAML.load(File.open(File.join(Rails.root, "config", "services.yml")))
+if Rails.env == "production"
+  puts "Loading Heroku variables"
+  
+  GOOGLE_MAPS_JAVASCRIPT_API_KEY  = ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"]
+  
+else
+  puts "Loading config variables from services.yml"
+  config = YAML.load(File.open(File.join(Rails.root, "config", "services.yml")))
 
-GOOGLE_MAPS_JAVASCRIPT_API_KEY  = config["google_maps"]["javascript_api_key"]
+  GOOGLE_MAPS_JAVASCRIPT_API_KEY  = config["google_maps"]["javascript_api_key"]
