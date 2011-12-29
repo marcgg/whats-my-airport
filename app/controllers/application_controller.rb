c4d464560411e7835f46bc0f8222fb3c5c49566a
Guillaume Flandre <guiome.flandre@gmail.com>
2011-12-29 23:24:15 +0100

    404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def not_found
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ AirportCodes::Application.routes.draw do
   root :to => "users#new"
   match "/:name/:id" => "users#show", :as => :result
   resources :users, :only => [:create]
+
+  match '*path', :controller => 'application', :action => 'not_found'
 end
