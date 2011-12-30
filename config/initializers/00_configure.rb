d98b933d668657e5ae106e7c26d032e2812643f3
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-12-30 20:32:52 +0100

    Do not assume that we have ENV on heroku

--- config/initializers/00_configure.rb
+++ config/initializers/00_configure.rb
@@ -1,12 +1,8 @@
 if Rails.env == "production"
   puts "Loading Heroku variables"
-  
-  GOOGLE_MAPS_JAVASCRIPT_API_KEY  = ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"]
-  
+  GOOGLE_MAPS_JAVASCRIPT_API_KEY  = ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"] if ENV
 else
   puts "Loading config variables from services.yml"
   config = YAML.load(File.open(File.join(Rails.root, "config", "services.yml")))
-
   GOOGLE_MAPS_JAVASCRIPT_API_KEY  = config["google_maps"]["javascript_api_key"]
-
 end
