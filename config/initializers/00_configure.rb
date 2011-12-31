4ded3fac635b56222df51678879af55b321e5292
Guillaume Flandre <guiome.flandre@gmail.com>
2011-12-31 11:12:29 +0100

    Google analytics setup #4

--- config/initializers/00_configure.rb
+++ config/initializers/00_configure.rb
@@ -2,11 +2,13 @@ if Rails.env == "production"
   puts "Loading Heroku variables"
   if ENV
     GOOGLE_MAPS_JAVASCRIPT_API_KEY = ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"]
+    GOOGLE_ANALYTICS_ACCOUNT_NUMBER = ENV["GOOGLE_ANALYTICS_ACCOUNT_NUMBER"]
     SHA1_PASSWORD = ENV["SHA1_PASSWORD"]
   end
 else
   puts "Loading config variables from services.yml"
   config = YAML.load(File.open(File.join(Rails.root, "config", "services.yml")))
   GOOGLE_MAPS_JAVASCRIPT_API_KEY  = config["google_maps"]["javascript_api_key"]
+  GOOGLE_ANALYTICS_ACCOUNT_NUMBER = config["google_analytics"]["account_number"]
   SHA1_PASSWORD = Digest::SHA1.hexdigest(config["wma"]["password"])
 end
--- lib/tasks/whatsmyairport.rake
+++ lib/tasks/whatsmyairport.rake
@@ -3,7 +3,7 @@ namespace :whatsmyairport do
   desc "Helper to set Heroku variables"
   task :heroku_vars => :environment do
     puts "Reference here: http://devcenter.heroku.com/articles/config-vars"
-    puts "heroku config:add GOOGLE_MAPS_JAVASCRIPT_API_KEY=#{GOOGLE_MAPS_JAVASCRIPT_API_KEY} SHA1_PASSWORD=#{SHA1_PASSWORD}"
+    puts "heroku config:add GOOGLE_MAPS_JAVASCRIPT_API_KEY=#{GOOGLE_MAPS_JAVASCRIPT_API_KEY} SHA1_PASSWORD=#{SHA1_PASSWORD} GOOGLE_ANALYTICS_ACCOUNT_NUMBER=#{GOOGLE_ANALYTICS_ACCOUNT_NUMBER}"
   end
 
 end
